92f4c35632f8be0fdaafac46007c0e995b4009ac
Yolanda S <50094135+yolandaas@users.noreply.github.com>
2019-11-18 15:30:20 -0800

    Add files via upload

--- subfreq.py
+++ subfreq.py
@@ -1,5 +1,6 @@
 from math import *
 import csv
+from aminoacids import *
 
 ###############
 # DESCRIPTION #
@@ -97,76 +98,12 @@ mid_col = -1
 sub_col = -1
 count_col = -1
 matrix = []
-labelled_matrix = []
 
 for i in range(20):
     lst = []
     for j in range(20):
         lst += [None]
     matrix.append(lst)
-
-Ala = 0
-Arg = 1
-Asn = 2
-Asp = 3
-Cys = 4
-Gln = 5
-Glu = 6
-Gly = 7
-His = 8
-Ile = 9
-Leu = 10
-Lys = 11
-Met = 12
-Phe = 13
-Pro = 14
-Ser = 15
-Thr = 16
-Trp = 17
-Tyr = 18
-Val = 19
-
-code ={"A" : Ala, 
-"R" : Arg,
-"D" : Asp, 
-"N" : Asn, 
-"C" : Cys,
-"Q" : Gln,
-"E" : Glu,
-"G" : Gly,
-"H" : His,
-"I" : Ile,
-"L" : Leu,
-"K" : Lys,
-"M" : Met,
-"F" : Phe,
-"P" : Pro,
-"S" : Ser,
-"T" : Thr,
-"W" : Trp,
-"Y" : Tyr,
-"V" : Val}
-
-G = Gly
-P = Pro
-A = Ala
-V = Val
-L = Leu
-I = Ile
-M = Met
-C = Cys
-F = Phe
-Y = Tyr
-W = Trp
-H = His
-K = Lys
-R = Arg
-Q = Gln
-N = Asn
-E = Glu
-D = Asp
-S = Ser
-T = Thr
 
 column_headers()
 csv_aggregator()
